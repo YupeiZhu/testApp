03596b4851bdf16ed17f6aa27a7c5ca01c693c42
Zhuyupei <yuz310@ucsd.edu>
2016-05-08 01:47:49 -0700

    done

---
+++ app/src/androidTest/java/team/myapplication/tests/JUnit_tests.java
@@ -0,0 +1,19 @@
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import team.myapplication.MainActivity;
+/**
+ * Created by Zhuyupei on 5/8/16.
+ */
+public class JUnit_tests extends ActivityInstrumentationTestCase2<MainActivity> {
+     MainActivity mainActivity;
+     public JUnit_tests(){
+         super(MainActivity.class);
+     }
+
+    public void test_first(){
+        mainActivity = getActivity();
+        assertEquals(mainActivity.add(10,10),20);
+    }
+}
--- app/src/main/java/team/myapplication/MainActivity.java
+++ app/src/main/java/team/myapplication/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add(1,2);
+    }
+
+    public int add(int a, int b){
+        return a+b;
     }
 }
